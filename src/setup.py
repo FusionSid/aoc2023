d6ee8878f47ec8d4632b8734398038558de89e2d
FusionSid <siddheshadsv@icloud.com>
2023-12-03 15:47:13 +1300

    fix: forgot to actually use the oneliner template i made :moyai:

--- src/setup.py
+++ src/setup.py
@@ -10,7 +10,7 @@ from rich import print
 
 
 SRC_DIR = path.dirname(__file__)
-ONELINE_TEMPLATE: Final = '[(importlib := __import__("importlib")), (sys := importlib.import_module("sys")), (os := __import__("os", globals(), locals(), ["path"], 0)), (path := os.path), (rich := __import__("rich", globals(), locals(), ["print"], 0)), (print := rich.print), (solve := (lambda data: print("Answer:", None))), [(use_test := (len(sys.argv) == 2 and sys.argv[-1] == "--test")), (input_path := path.join(path.dirname(__file__), "input.txt" if not use_test else "test.txt")), (data := open(input_path).readlines()), solve(data)] if __name__ == "__main__" else ...]  # type: ignore'  # noqa
+ONELINE_TEMPLATE: Final = '[(importlib := __import__("importlib")), (sys := importlib.import_module("sys")), (os := __import__("os", globals(), locals(), ["path"], 0)), (path := os.path), (rich := __import__("rich", globals(), locals(), ["print"], 0)), (print := rich.print), (solve := (lambda data: print("Answer:", None))), [(use_test := (len(sys.argv) == 2 and sys.argv[-1] == "--test")), (input_path := path.join(path.dirname(__file__), "input.txt" if not use_test else "test.txt")), (data := open(input_path).readlines()), solve(data)] if __name__ == "__main__" else ...]  # type: ignore\n'  # noqa
 
 
 def save_input(day: str, dir_path: str) -> None:
@@ -40,7 +40,14 @@ def main(day: str):
 
     additional_files = ["p1.oneline.py", "p2.oneline.py", "test.txt"]
     for file in additional_files:
-        filepath = Path(path.join(DIRECTORY, file))
+        fp = path.join(DIRECTORY, file)
+
+        if "oneline" in file:
+            with open(fp, "w") as f:
+                f.write(ONELINE_TEMPLATE)
+            continue
+
+        filepath = Path(fp)
         filepath.touch()
     print("[b green]Additional Files Created!")
 
