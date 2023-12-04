43a832ba3738ad39903bf59fa13fe6d1acc049bd
FusionSid <siddheshadsv@icloud.com>
2023-12-04 19:29:03 +1300

    fix: template not splitting lines properly

--- src/setup.py
+++ src/setup.py
@@ -10,7 +10,7 @@ from rich import print
 
 
 SRC_DIR = path.dirname(__file__)
-ONELINE_TEMPLATE: Final = '[(importlib := __import__("importlib")), (sys := importlib.import_module("sys")), (os := __import__("os", globals(), locals(), ["path"], 0)), (path := os.path), (rich := __import__("rich", globals(), locals(), ["print"], 0)), (print := rich.print), (solve := (lambda data: print("Answer:", None))), [(use_test := (len(sys.argv) == 2 and sys.argv[-1] == "--test")), (input_path := path.join(path.dirname(__file__), "input.txt" if not use_test else "test.txt")), (data := open(input_path).readlines()), solve(data)] if __name__ == "__main__" else ...]  # type: ignore\n'  # noqa
+ONELINE_TEMPLATE: Final = r'[(importlib := __import__("importlib")), (sys := importlib.import_module("sys")), (os := __import__("os", globals(), locals(), ["path"], 0)), (path := os.path), (rich := __import__("rich", globals(), locals(), ["print"], 0)), (print := rich.print), (solve := (lambda data: print("Answer:", None))), [(use_test := (len(sys.argv) == 2 and sys.argv[-1] == "--test")), (input_path := path.join(path.dirname(__file__), "input.txt" if not use_test else "test.txt")), (data := open(input_path).read().split("\n")), solve(data)] if __name__ == "__main__" else ...]'  # noqa
 
 
 def save_input(day: str, dir_path: str) -> None:
